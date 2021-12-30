11672b04469dd96f537d97bfaf4636cb8eb7f35a
Evgen Zaytsev <zond.jeka@gmail.com>
2021-12-30 04:44:13 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -6,7 +6,7 @@ CREATE TABLE Personage
 	pers_name	char(30) NOT NULL,
 	pers_weapon	char(20) NOT NULL,
 	pers_feature	char(20) NOT NULL,
-	pers_main_role	char(20) NULL
+	pers_main_role	char(20) NOT NULL
 );
 
 -----------------------
@@ -25,21 +25,12 @@ CREATE TABLE Feature
 	feature_name	char(20) NOT NULL
 );
 
--------------------------
--- Create Main_role table
--------------------------
-CREATE TABLE Main_role
-(
-	main_role_name	char(20) NOT NULL
-);
-
 ------------------------------
 -- Create Main_role_list table
 ------------------------------
 CREATE TABLE Main_role_list
 (
-	main_role_name char(20) NOT NULL,
-	pers_main_role char(20) NOT NULL
+	main_role_name char(20) NOT NULL
 );
 
 ----------------------
@@ -48,8 +39,7 @@ CREATE TABLE Main_role_list
 ALTER TABLE Personage ADD CONSTRAINT PK_Personage PRIMARY KEY (pers_name);
 ALTER TABLE Weapon ADD CONSTRAINT PK_Weapon PRIMARY KEY (weapon_name);
 ALTER TABLE Feature ADD CONSTRAINT PK_Feature PRIMARY KEY (feature_name);
-ALTER TABLE Main_role ADD CONSTRAINT PK_main_role PRIMARY KEY (main_role_name);
---ALTER TABLE Main_role_list ADD CONSTRAINT PK_main_role_list PRIMARY KEY (main_role_name);
+ALTER TABLE Main_role_list ADD CONSTRAINT PK_main_role PRIMARY KEY (main_role_name);
 
 ----------------------
 -- Define foreign keys
@@ -60,5 +50,3 @@ ALTER TABLE Personage ADD CONSTRAINT FK_Personage_Feature FOREIGN KEY (pers_feat
 REFERENCES Feature (feature_name);
 ALTER TABLE Personage ADD CONSTRAINT FK_Personage_Main_role_list FOREIGN KEY (pers_main_role)
 REFERENCES Main_role_list (main_role_name);
-ALTER TABLE Main_role_list ADD CONSTRAINT FK_Main_role_list_Main_role FOREIGN KEY (pers_main_role)
-REFERENCES Main_role (main_role_name);
