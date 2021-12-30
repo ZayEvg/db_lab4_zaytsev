ca9694b230daa0e115f9383af883bde3beb35a3b
Evgen Zaytsev <zond.jeka@gmail.com>
2021-12-30 04:44:36 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,4 +1,32 @@
 ---------------------------
+-- Populate Weapon table
+---------------------------
+INSERT INTO Weapon(weapon_name) VALUES('Polearm');
+INSERT INTO Weapon(weapon_name) VALUES('Sword');
+INSERT INTO Weapon(weapon_name) VALUES('Claymore');
+INSERT INTO Weapon(weapon_name) VALUES('Catalyst');
+INSERT INTO Weapon(weapon_name) VALUES('Bow');
+
+---------------------------
+-- Populate Feature table
+---------------------------
+INSERT INTO Feature(feature_name) VALUES('Anemo');
+INSERT INTO Feature(feature_name) VALUES('Geo');
+INSERT INTO Feature(feature_name) VALUES('Electro');
+INSERT INTO Feature(feature_name) VALUES('Hydro');
+INSERT INTO Feature(feature_name) VALUES('Pyro');
+INSERT INTO Feature(feature_name) VALUES('Cryo');
+INSERT INTO Feature(feature_name) VALUES('Dendro');
+
+---------------------------
+-- Populate Main_role table
+---------------------------
+INSERT INTO Main_role_list(main_role_name) VALUES('DPS');
+INSERT INTO Main_role_list(main_role_name) VALUES('Sub DPS');
+INSERT INTO Main_role_list(main_role_name) VALUES('Support');
+INSERT INTO Main_role_list(main_role_name) VALUES('Healer');
+
+---------------------------
 -- Populate Personage table
 ---------------------------
 INSERT INTO Personage(pers_name, pers_weapon, pers_feature, pers_main_role) 
@@ -21,46 +49,3 @@ INSERT INTO Personage(pers_name, pers_weapon, pers_feature, pers_main_role)
 VALUES('Zhongli', 'Polearm', 'Geo', 'Support');
 INSERT INTO Personage(pers_name, pers_weapon, pers_feature, pers_main_role) 
 VALUES('Raiden Shogun', 'Polearm', 'Electro', 'Support');
-
----------------------------
--- Populate Weapon table
----------------------------
-INSERT INTO Weapon(weapon_name) VALUES('Sword');
-INSERT INTO Weapon(weapon_name) VALUES('Claymore');
-INSERT INTO Weapon(weapon_name) VALUES('Catalyst');
-INSERT INTO Weapon(weapon_name) VALUES('Bow');
-INSERT INTO Weapon(weapon_name) VALUES('Polearm');
-
----------------------------
--- Populate Feature table
----------------------------
-INSERT INTO Feature(feature_name) VALUES('Anemo');
-INSERT INTO Feature(feature_name) VALUES('Geo');
-INSERT INTO Feature(feature_name) VALUES('Electro');
-INSERT INTO Feature(feature_name) VALUES('Hydro');
-INSERT INTO Feature(feature_name) VALUES('Pyro');
-INSERT INTO Feature(feature_name) VALUES('Cryo');
-INSERT INTO Feature(feature_name) VALUES('Dendro');
-
----------------------------
--- Populate Main_role table
----------------------------
-INSERT INTO Main_role(main_role_name) VALUES('DPS');
-INSERT INTO Main_role(main_role_name) VALUES('Sub DPS');
-INSERT INTO Main_role(main_role_name) VALUES('Support');
-INSERT INTO Main_role(main_role_name) VALUES('Healer');
-
----------------------------
--- Populate Main_role table
----------------------------
-INSERT INTO Main_role_list(main_role_name, pers_main_role) VALUES('Arataki Itto', 'DPS');
-INSERT INTO Main_role_list(main_role_name, pers_main_role) VALUES('Mona', 'Sub DPS');
-INSERT INTO Main_role_list(main_role_name, pers_main_role) VALUES('Mona', 'Support');
-INSERT INTO Main_role_list(main_role_name, pers_main_role) VALUES('Raiden Shogun', 'Sub DPS');
-INSERT INTO Main_role_list(main_role_name, pers_main_role) VALUES('Raiden Shogun', 'Support');
-INSERT INTO Main_role_list(main_role_name, pers_main_role) VALUES('Zhongli', 'DPS');
-INSERT INTO Main_role_list(main_role_name, pers_main_role) VALUES('Zhongli', 'Support');
-INSERT INTO Main_role_list(main_role_name, pers_main_role) VALUES('Bennet', 'DPS');
-INSERT INTO Main_role_list(main_role_name, pers_main_role) VALUES('Bennet', 'Support');
-INSERT INTO Main_role_list(main_role_name, pers_main_role) VALUES('Fischl', 'Sub DPS');
-INSERT INTO Main_role_list(main_role_name, pers_main_role) VALUES('Fischl', 'Support');
